7a88fa1ae89e13f728e158e8a35f157de20ea448
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-07-17 13:53:37 -0500

    converted 2nd tier prompts to single switch case

--- index.js
+++ index.js
@@ -25,91 +25,54 @@ function viewDepartment() {
         })
     )};
 
-// 2nd tier prompt (c)
-async function updateWhat (views) {
-    inquirer
-        .prompt({
-            name: "updatePrompt",
-            type: "list",
-            message: "What type of items would you like to update in the database?",
-            choices: ['Departments', 'Roles', 'Employees']
-        })
-        .then(function (answer) {
-        
-            let key = answer.updatePrompt;
 
-            switch (key) {
-                case "Departments": viewDepartment(); break;
-                case "Roles": return console.log(`roles`);
-                case "Employees": return console.log(`employees`); //SELECT * from employees where name = {}, 
-                default: return connection.end();;//UPDATE D/R/E SET answer  = {}
-            }
-        }); // EDIT OR DELETE
-};
-// 2nd tier prompt (b)
-async function viewWhat (views) {
-    inquirer
-        .prompt({
-            name: "viewPrompt",
-            type: "list",
-            message: "What type of items would you like to view from the database?",
-            choices: ['Departments', 'Roles', 'Employees']
-        })
-        .then(function (answer) {
-        
-            let key = answer.viewPrompt;
-
-            switch (key) {
-                case "Departments": viewDepartment(); break;
-                case "Roles": return console.log(`roles`);
-                case "Employees": return console.log(`employees`);
-                default: return connection.end();;
-            }
-        });
-};
-// 2nd tier prompt (a)
-async function addWhat (adds) {
-    inquirer
-        .prompt({
-            name: "addPrompt",
-            type: "list",
-            message: "Which type would you like to add to the database?",
-            choices: ['Add a department', 'Add a role', 'Add an employee']
-        })
-        .then(function (answer) {
-        
-            let key = answer.addPrompt;
-
-            switch (key) {
-                case "Add a department": return console.log(`departments`);
-                case "Add a role": return console.log(`roles`);
-                case "Add an employee": return console.log(`employees`);
-                default: return connection.end();;
-            }
-        });
-};
 // initial prompt
 function start() {
     inquirer
         .prompt({
             name: "firstPrompt",
             type: "list",
-            message: "Welcome to the Employees Management System! What would you like to do?",
+            message: "Welcome to the Employees Management System! What task would you like to perform?",
             choices: [
-                "Add departments, roles, or employees", 
-                "View departments, roles, or employees",
-                "Update an employee role"]
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "View Employees by Manager",
+            "View Total Budget by Department",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role",
+            "Remove a department",
+            "Remove an employee", 
+            "Exit"
+        ]
         })
         .then(function (answer) {
            
             let key = answer.firstPrompt;
 
             switch (key) {
-                case "Add departments, roles, or employees": addWhat();break;
-                case "View departments, roles, or employees": viewWhat();break;
-                case "Update an employee role": updateWhat();break;
+                case "View all departments": deptView();break;
+                case "View all roles": roleView();break;
+                case "View all employees": empView();break;
+                case "View Employees by Manager": managerEmpView();break;
+                case "View Total Budget by Department": totalBudget();break;// * View the total utilized budget of a department -- ie the combined salaries of all employees in that department
+                case "Add a department": addDept();break;
+                case "Add a role": addRole();break;
+                case "Add an employee": addEmp();break;
+                case "Update an employee role": updateEmpRole();break;
+                case "Remove a department": deleteDept();break;
+                case "Remove a role": deleteDept();break;
+                case "Remove an employee": deleteDept();break;
+                case "Exit": start() ;break;
                 default: break;
             }
+            // Update employee managers
+            // * View employees by manager
+            // * Delete departments, roles, and employees
+            
+          
         });
 };
 
