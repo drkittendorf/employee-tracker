ab106275c4de72cbb6ae4787ce0a84919df69669
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-07-16 19:34:58 -0500

    schema running clean

--- Db/employee.sql
+++ Db/employee.sql
@@ -1,38 +1,30 @@
-ALTER USER 'root'@'localhost' IDENTIFIED
-WITH mysql_native_password BY 'Erikmysql';
-
-CREATE DATABASE employeeDB;
-
-USE employeeDB;
-
---  DEPARTMENT
 
 
--- CREATING ROLE TABLE  
-CREATE TABLE role (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    title TEXT NOT NULL,
-    salary DECIMAL NOT NULL,
-)
+CONSTRAINT fk_role_deptID FOREIGN KEY(department_id) REFERENCES department (id)
 -- CREATING EMPLOYEE TABLE 
 create table employee
 (
-    id int primary key not null
-    auto_increment,
+    id int primary key not null auto_increment,
     name text not null,
     manager_id int,
-    constraint fk_employee foreign key
-    (manager_id) references employee
-    (id)
+    CONSTRAINT fk_employee foreign key (manager_id) references employee(id)
 );
 
-    insert into employee
-        (name, manager_id)
+
+
+    insert into employee (name, manager_id)
     values('Mark', 1),
-        ('Ariel', 2),
-        ('Samir', 2),
-        ('Ty', 1);
-    select A.name as 'Employee',
-        B.name as 'Manager'
-    from employee as A
-        Inner Join employee as B on A.manager_id = B.id;
+          ('Ariel', 2),
+          ('Samir', 2),
+          ('Ty', 1);
+-- select A.name as 'Employee',
+--     B.name as 'Manager'
+-- from employee as A
+--     Inner Join employee as B on A.manager_id = B.id;
+* **employee**:
+
+  * **id** - INT PRIMARY KEY
+  * **first_name** - VARCHAR(30) to hold employee first name
+  * **last_name** - VARCHAR(30) to hold employee last name
+  * **role_id** - INT to hold reference to role employee has
+  * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
--- Db/schema.sql
+++ Db/schema.sql
@@ -1,66 +1,25 @@
---CODE FROM JEFF
-
--- create table employee (
--- id int primary key not null auto_increment,
--- name text not null,
--- manager_id int,
--- constraint fk_employee foreign key (manager_id)
--- references employee(id)
--- );
-insert into employee
-    (name, manager_id)
-values('Mark', 1),
-    ('Ariel', 2),
-    ('Samir', 2),
-    ('Ty', 1);
-select A.name as 'Employee', B.name as 'Manager'
-from employee as A
-    Inner Join employee as B on A.manager_id = B.id;
-
---END OF CODE FROM JEFF
-
-
-ALTER USER 'root'@'localhost' IDENTIFIED
-WITH mysql_native_password BY 'Erikmysql';
+DROP DATABASE IF EXISTS employeeDB;
 
 CREATE DATABASE employeeDB;
 
 USE employeeDB;
-
-
---  DEPARTMENT 
-
-
-
--- CREATING ROLE TABLE  
-CREATE TABLE role (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    title TEXT NOT NULL,
-    salary DECIMAL NOT NULL,
-
-)
-
--- CREATING EMPLOYEE TABLE 
-create table employee
-(
-    id int primary key not null
-    auto_increment,
-    name text not null,
-    manager_id int,
-    constraint fk_employee foreign key
-    (manager_id) references employee
-    (id)
+show tables;
+CREATE TABLE employee (
+    employee_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
 );
 
+CREATE TABLE department (
+    department_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name TEXT NOT NULL
+);
 
-
-    insert into employee
-        (name, manager_id)
-    values('Mark', 1),
-        ('Ariel', 2),
-        ('Samir', 2),
-        ('Ty', 1);
-    select A.name as 'Employee',
-        B.name as 'Manager'
-    from employee as A
-        Inner Join employee as B on A.manager_id = B.id;
+CREATE TABLE role (
+    role_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title TEXT NOT NULL,
+    salary DECIMAL NOT NULL,
+    deparment_id INT
+);
